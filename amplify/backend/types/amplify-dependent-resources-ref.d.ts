c2bfe2eba6d2cb19333e13cc8118de5df458d9bb
Sebastian GM1091 <sebastianleonardo299@gmail.com>
2025-08-02 23:05:26 -0500

    test

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "educatelabs": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
   "auth": {
     "educatelabs673e18d9": {
       "AppClientID": "string",
